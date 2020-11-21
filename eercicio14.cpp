97a6c2dba04b766117ea61a22fa889ccb31e7e4f
Louernce <lqfmecatronica@gmail.com>
2020-11-21 17:07:01 -0300

    Vetor com valores repetidos resolvido

--- eercicio14.cpp
+++ eercicio14.cpp
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+Fazer um programa para ler 5 valores e, em seguida, 
+mostrar a posic¸ao onde se encontrar 
+o maior e o menor valor.
+
+*/
 int main()
 {
     int a, repetido, b, i;
-    int vet[10],vet_i[10];
+    int vet[5],vet_i[5];
 
-    printf("digite 10 vet");
+    printf("digite 5 vet");
     printf("\n");
     
-    for (a=0;a<10;a++)
+    for (a=0;a<5;a++)
     {
       printf("nota %d : ",a+1);
       scanf("%d", &vet[a]);
-      
-      for(a=1;a<=10;a++)
+    }
+		
+      for(a=1;a<=5;a++)
 	  {
-      	for(b=1;b<=10;b++)
+      	for(b=1;b<=5;b++)
 		  {
       		if(vet[a]==vet[b])
       		{
@@ -24,12 +31,13 @@ int main()
 		  }
 	  }
       
-	}
+	
 	printf("Eis o algarismos iguais:\n");
-	for (a=0;a<10;a++)
+	for (a=0;a<5;a++)
     {
-      printf("%d: ",vet[a]);     
+      printf("%d: ",vet[repetido]);     
 	} 
 	
     return 0;
 }
+
---
+++ exercicio_14.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Fazer um programa para ler 5 valores e, em seguida, 
+mostrar a posic¸ao onde se encontrar 
+o maior e o menor valor.
+
+*/
+int main()
+{
+    int a, repetido, b, i;
+    int vet[5],vet_i[5];
+
+    printf("digite 5 vet");
+    printf("\n");
+    
+    for (a=0;a<5;a++)
+    {
+      printf("nota %d : ",a+1);
+      scanf("%d", &vet[a]);
+    }
+		
+      for(a=1;a<=5;a++)
+	  {
+      	for(b=0;b<a;b++)
+		  {
+      		if(vet[a]==vet[b])
+      		{
+      			   printf("%d foi repetido! Na posição %d\n",vet[a],a);     	  
+      		}
+		  }
+	  }
+      
+	/*
+	printf("Eis o algarismos iguais:\n");
+	for (a=0;a<5;a++)
+    {
+      printf("%d: ",vet[repetido]);     
+	} 
+	*/
+    return 0;
+}
